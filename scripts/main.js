82d3cee7d9fdbea461103b1ae7cd4d7fa59fc672
Stephen Allard <stephencallard@gmail.com>
2021-10-05 22:26:23 -0500

    add books to library by submitting form data

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,11 @@
 let libraryArr = [];
-let isAdd = true;
+let addBookButton = document.querySelector("#addBook");
 
 function Book(title, author, numPages, isRead) {
   this.title = title;
   this.author = author;
   this.numPages = numPages;
   this.isRead = isRead;
-}
-
-function addBookToLibrary() {
-  let title = prompt("Enter a book title");
-  let author = prompt("Enter the book's author");
-  let numPages = prompt("How many pages is the book?");
-
-  let userInput = prompt("Have you read the book?  Enter yes/no");
-  let isRead;
-  if (userInput === "yes") {
-    isRead = true;
-  } else {
-    isRead = false;
-  }
-
-  let newBook = new Book(title, author, numPages, isRead);
-  libraryArr.push(newBook);
 }
 
 function displayLibrary() {
@@ -55,14 +38,23 @@ function createBookCard(book) {
   return card;
 }
 
-while(isAdd) {
-  addBookToLibrary();
-  userInput = prompt('Would you like to add another book to the library? Enter yes/no');
-  if(userInput === "no") {
-    isAdd = false;
-  }
-}
-displayLibrary();
+document.querySelector('form').addEventListener("submit", event => {
+  var data = new FormData(event.target);
+  var bookInfo = data.getAll("book");
+  var title = bookInfo[0];
+  var author = bookInfo[1];
+  var length = parseInt(bookInfo[2]);
+  var hasRead = (bookInfo[3] === "true");
+ 
+  let book = new Book(title,author,length,hasRead);
+  libraryArr.push(book);
+  displayLibrary();
+
+  event.preventDefault()
+})
+
+
+
 
 
 
